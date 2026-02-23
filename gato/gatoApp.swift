d8fd35995f6dee24e7a020a8e5c26c963615443c
JoseFigueiredo <jmglfigueiredo@gmail.com>
2026-02-23 12:46:25 +0000

    Initial Commit

---
+++ gato/gatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  gatoApp.swift
+//  gato
+//
+//  Created by José Miguel Figueiredo on 23/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct gatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
